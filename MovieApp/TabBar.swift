8d2c206caf1f76702691c038ae6cc2c7a174a102
shanidzeann <shanidzeann@mail.ru>
2022-02-21 19:47:18 +0300

    Create tab bar

--- MovieApp/TabBar.swift
+++ MovieApp/TabBar.swift
@@ -12,18 +12,37 @@ class TabBar: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 29/255, green: 24/255, blue: 36/255, alpha: 1)
+        UITabBar.appearance().barTintColor = UIColor(red: 189/255, green: 46/255, blue: 63/255, alpha: 1)
+        tabBar.backgroundColor = UIColor(red: 189/255, green: 46/255, blue: 63/255, alpha: 1)
+        tabBar.tintColor = .white
+        setupVCs()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupVCs() {
+        viewControllers = [
+            createNavController(for: HomeViewController(), title: "Home", image: UIImage(systemName: "house.fill")!),
+            createNavController(for: HomeViewController(), title: "Hz", image: UIImage(systemName: "play.circle")!)
+        ]
     }
-    */
-
+    
+    private func createNavController(for rootViewController: UIViewController,
+                                     title: String,
+                                     image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        rootViewController.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = false
+        
+        let button = UIButton(configuration: .plain(), primaryAction: nil)
+        var config = UIButton.Configuration.plain()
+        config.image = UIImage(systemName: "magnifyingglass")
+        button.configuration = config
+        button.tintColor = .white
+        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        rootViewController.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: button)
+        
+        return navController
+    }
 }
