9b26d90ebcc5096c45c634fd2a42c8ee75e6bb5d
shanidzeann <shanidzeann@mail.ru>
2022-02-22 20:36:51 +0300

    Get three lists of movies (URLSession, DispatchGroup)

--- MovieApp/Managers/NetworkManager.swift
+++ MovieApp/Managers/NetworkManager.swift
@@ -9,23 +9,40 @@ import Foundation
 
 class NetworkManager {
     
-    let popularMoviesURL = "https://api.themoviedb.org/3/movie/popular?api_key=\(yourKey)&language=en-US&page=1"
+    let popularMoviesURL = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=\(yourKey)&language=en-US&page=1")
+    let topRatedURL = URL(string: "https://api.themoviedb.org/3/movie/top_rated?api_key=\(yourKey)&language=en-US&page=1")
+    let upcoming = URL(string: "https://api.themoviedb.org/3/movie/upcoming?api_key=\(yourKey)&language=en-US&page=1")
     
-    func getMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
-        guard let url = URL(string: popularMoviesURL) else { return }
-        let task = URLSession.shared.dataTask(with: url) { data, _, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data {
-                do {
-                    let result = try JSONDecoder().decode(List.self, from: data)
-                    completion(.success(result.results))
-                } catch {
-                    completion(.failure(error))
+    func downloadUrls(completion: @escaping (Result<[List], Error>) -> Void) {
+        guard let popularMoviesURL = popularMoviesURL,
+              let topRatedURL = topRatedURL,
+              let upcoming = upcoming else { return }
+        
+        let urls = [popularMoviesURL, topRatedURL, upcoming]
+        var movieCollection: [List] = []
+        let urlDownloadQueue = DispatchQueue(label: "com.urlDownloader.urlqueue")
+        let urlDownloadGroup = DispatchGroup()
+        
+        urls.forEach { url in
+            urlDownloadGroup.enter()
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                guard let data = data,
+                      let result = try? JSONDecoder().decode(List.self, from: data) else {
+                          completion(.failure(error!))
+                          urlDownloadQueue.async {
+                              urlDownloadGroup.leave()
+                          }
+                          return
+                      }
+                urlDownloadQueue.async {
+                    movieCollection.append(result)
+                    urlDownloadGroup.leave()
                 }
-            }
+            }.resume()
         }
-        task.resume()
+        
+        urlDownloadGroup.notify(queue: DispatchQueue.global()) {
+            completion(.success(movieCollection))
+        }
     }
-    
 }
