d792eee5679d89b2d24218daf91419b7628fcfbe
shanidzeann <shanidzeann@mail.ru>
2022-02-24 17:13:15 +0300

    Create header for cast collection view

---
+++ MovieApp/View/CastHeaderSupplementaryView.swift
@@ -0,0 +1,39 @@
+//
+//  CastHeaderSupplementaryView.swift
+//  MovieApp
+//
+//  Created by Anna Shanidze on 24.02.2022.
+//
+
+import UIKit
+import SnapKit
+
+class CastHeaderSupplementaryView: UICollectionReusableView {
+        
+    private let label: UILabel = {
+       let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 19)
+        label.textColor = .white
+        label.text = "Cast"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        createUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createUI() {
+        addSubview(label)
+        label.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.left.equalToSuperview().inset(10)
+        }
+    }
+    
+}
