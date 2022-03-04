9d87c59ade9d66f74761f368625dcb61aacf8f70
shanidzeann <shanidzeann@mail.ru>
2022-03-05 01:03:38 +0300

    Refactor

--- MovieAppTests/HomeModuleTest/HomePresenterTest.swift
+++ MovieAppTests/HomeModuleTest/HomePresenterTest.swift
@@ -49,9 +49,6 @@ class HomePresenterTest: XCTestCase {
     }
     
     func testGetFailureMovieLists() {
-        let movie = Movie(adult: nil, backdropPath: "", genreIDS: [1], id: 1, originalLanguage: nil, originalTitle: nil, overview: nil, popularity: nil, posterPath: "", releaseDate: "", title: "", video: nil, voteAverage: 1.0, voteCount: 1)
-        let movies = List(dates: nil, page: 1, results: [movie], totalPages: 1, totalResults: 1)
-        lists.append((url: "Foo", movies: movies))
         networkManager = MockNetworkManager()
         presenter = HomePresenter(view: view, networkManager: networkManager, router: router)
         
