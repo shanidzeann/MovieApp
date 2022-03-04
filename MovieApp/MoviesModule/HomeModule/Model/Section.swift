8d6863a6d8b3b02cf5e0dfe076c807564bae4055
shanidzeann <shanidzeann@mail.ru>
2022-03-04 16:23:14 +0300

    Move section enum to a separate file

---
+++ MovieApp/MoviesModule/HomeModule/Model/Section.swift
@@ -0,0 +1,14 @@
+//
+//  Section.swift
+//  MovieApp
+//
+//  Created by Anna Shanidze on 04.03.2022.
+//
+
+import Foundation
+
+enum Section: Int, CaseIterable {
+    case popular
+    case topRated
+    case upcoming
+}
