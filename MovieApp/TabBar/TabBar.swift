40d37b3e039e42ba80284606c0f697bb524dd0dd
shanidzeann <shanidzeann@mail.ru>
2022-03-04 18:42:30 +0300

    Delete custom view of tab bar

--- MovieApp/TabBar/TabBar.swift
+++ MovieApp/TabBar/TabBar.swift
@@ -12,7 +12,6 @@ class TabBar: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setColors()
-        setCornerRadius()
     }
     
     private func setColors() {
@@ -20,30 +19,5 @@ class TabBar: UITabBarController {
         tabBar.backgroundColor = UIColor(red: 189/255, green: 46/255, blue: 63/255, alpha: 1)
         tabBar.tintColor = .white
     }
-    
-    private func setCornerRadius() {
-        tabBar.layer.masksToBounds = true
-        tabBar.layer.cornerRadius = 30
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        tabBar.frame = CGRect(x: tabBar.frame.origin.x + 15,
-                              y: tabBar.frame.origin.y - 30,
-                              width: tabBar.frame.size.width - 30,
-                              height: tabBar.frame.size.height + 10)
-    }
-    
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        let buttons = tabBar.subviews.filter { String(describing: type(of: $0)) == "UITabBarButton" }
-        buttons.forEach {
-            if let superviewHeight = $0.superview?.frame.height {
-                $0.center = CGPoint(x: $0.frame.midX, y: superviewHeight/2 - 5)
-            }
-        }
-    }
-    
     
 }
