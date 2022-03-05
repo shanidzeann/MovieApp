eca39dbc19d66c72f4f8c142dadd9a52601e2ba9
shanidzeann <shanidzeann@mail.ru>
2022-03-05 19:09:49 +0300

    Create DetailModuleTest

---
+++ MovieAppTests/MoviesModuleTest/DetailModuleTest/DetailPresenterTest.swift
@@ -0,0 +1,14 @@
+//
+//  DetailPresenterTest.swift
+//  MovieAppTests
+//
+//  Created by Anna Shanidze on 05.03.2022.
+//
+
+import XCTest
+@testable import MovieApp
+
+class DetailPresenterTest: XCTestCase {
+    
+    
+}
